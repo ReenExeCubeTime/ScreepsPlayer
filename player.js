21582a1f60a5fa26dc605504be015b59f8ad1e92
ReenExe <ipourri@gmail.com>
2017-01-13 23:50:21 +0200

    try harvest

--- player.js
+++ player.js
@@ -1,7 +1,14 @@
 module.exports.loop = function () {
     for (let index in Game.creeps) {
-        global.console.log(index);
+        console.log(index);
         let creep = Game.creeps[index];
         creep.say(`Some say`);
+
+        const sources = creep.room.find(FIND_SOURCES);
+        const source = sources[0];
+
+        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+            console.log('need walk');
+        }
     }
 };
