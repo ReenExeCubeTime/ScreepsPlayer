bd75955493d8f3a7805c333d67bc194de18b5f98
ReenExe <ipourri@gmail.com>
2017-01-14 02:11:12 +0200

    store energy

--- player.js
+++ player.js
@@ -1,14 +1,18 @@
 module.exports.loop = function () {
     for (let index in Game.creeps) {
-        console.log(index);
         let creep = Game.creeps[index];
-        creep.say(`Some say`);
+        if (creep.carry.energy < creep.carryCapacity) {
+            const sources = creep.room.find(FIND_SOURCES);
+            const source = sources[0];
 
-        const sources = creep.room.find(FIND_SOURCES);
-        const source = sources[0];
-
-        if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
+            if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+        } else {
+            const spawn = Game.spawns['Spawn1'];
+            if (creep.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawn);
+            }
         }
     }
 };
